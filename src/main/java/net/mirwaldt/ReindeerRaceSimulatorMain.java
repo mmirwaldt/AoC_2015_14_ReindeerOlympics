28fd0753c93df97bd0e259fcc604ad2c7be49a11
mmirwaldt <michael.mirwaldt@gmail.com>
2021-02-26 23:58:48 +0100

    solved part two of the puzzle

--- src/main/java/net/mirwaldt/ReindeerRaceSimulatorMain.java
+++ src/main/java/net/mirwaldt/ReindeerRaceSimulatorMain.java
@@ -12,7 +12,29 @@ import static net.mirwaldt.ReindeerRaceSimulator.simulateRaceAndReturnWinningDis
 public class ReindeerRaceSimulatorMain {
     public static void main(String[] args) throws IOException {
         final List<String> lines = Files.readAllLines(Path.of("input.txt"), StandardCharsets.US_ASCII);
-        final ReindeerRaceSimulator reindeerRaceSimulator = new DistanceReindeerRaceSimulator();
+        final ReindeerRaceSimulator reindeerRaceSimulatorForPartOne = new DistanceReindeerRaceSimulator();
+        addReindeers(lines, reindeerRaceSimulatorForPartOne);
+
+        final Map<String, Integer> raceResultsForPartOne = reindeerRaceSimulatorForPartOne.simulateRace(2503);
+
+        // {Donner=2655, Vixen=2640, Comet=2493, Prancer=2484, Dasher=2460, Blitzen=2496, Dancer=2516, Rudolph=2540, Cupid=2592}
+        System.out.println(raceResultsForPartOne);
+        // 2655
+        System.out.println(simulateRaceAndReturnWinningDistance(raceResultsForPartOne));
+
+
+        final ReindeerRaceSimulator reindeerRaceSimulatorForPartTwo = new LeadScoringReindeerRaceSimulator();
+        addReindeers(lines, reindeerRaceSimulatorForPartTwo);
+
+        final Map<String, Integer> raceResultsPartTwo = reindeerRaceSimulatorForPartTwo.simulateRace(2503);
+
+        // {Donner=414, Vixen=1059, Comet=22, Prancer=153, Dasher=0, Blitzen=5, Dancer=1, Rudolph=887, Cupid=13}
+        System.out.println(raceResultsPartTwo);
+        // 1059
+        System.out.println(simulateRaceAndReturnWinningDistance(raceResultsPartTwo));
+    }
+
+    private static void addReindeers(List<String> lines, ReindeerRaceSimulator reindeerRaceSimulator) {
         for (String line : lines) {
             final String[] tokens = line.split(" ");
             final String name = tokens[0];
@@ -21,14 +43,5 @@ public class ReindeerRaceSimulatorMain {
             final int restTimeInSeconds = Integer.parseInt(tokens[13]);
             reindeerRaceSimulator.addReindeer(name, velocityInKmPerSecond, flyingTimeInSeconds, restTimeInSeconds);
         }
-
-        final Map<String, Integer> raceResults = reindeerRaceSimulator.simulateRace(2503);
-
-        // {Donner=2655, Vixen=2640, Comet=2493, Prancer=2484, Dasher=2460, Blitzen=2496, Dancer=2516, Rudolph=2540, Cupid=2592}
-        System.out.println(raceResults);
-        // 2655
-        System.out.println(simulateRaceAndReturnWinningDistance(raceResults));
-
-
     }
 }
