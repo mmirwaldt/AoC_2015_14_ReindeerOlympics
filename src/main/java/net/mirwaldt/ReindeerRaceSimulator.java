b4da86adf960c909047e4caae4f847a034d8f272
mmirwaldt <michael.mirwaldt@gmail.com>
2021-02-26 23:18:35 +0100

    removed unused method

--- src/main/java/net/mirwaldt/ReindeerRaceSimulator.java
+++ src/main/java/net/mirwaldt/ReindeerRaceSimulator.java
@@ -5,10 +5,6 @@ import java.util.Map;
 public interface ReindeerRaceSimulator {
     void addReindeer(String name, int velocityInKmPerSecond, int flyingTimeInSeconds, int restTimeInSeconds);
     Map<String, Integer> simulateRace(int timeInSeconds);
-    default int simulateRaceAndReturnWinningDistance(int timeInSeconds) {
-        return simulateRaceAndReturnWinningDistance(simulateRace(timeInSeconds));
-    }
-
     static int simulateRaceAndReturnWinningDistance(Map<String, Integer> raceResults) {
         return raceResults.values().stream().mapToInt(Integer::intValue).max().getAsInt();
     }
